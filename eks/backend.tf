a57e78b6319b6d21aca078a04e3a9af133c4d83e
Praduman Prajapati <praduman.cnd@gmail.com>
2025-03-07 16:26:26 +0530

    modified some files

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,7 +7,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "dev-aman-tf-bucket"
+    bucket         = "praduman-eks-s3"
     region         = "ap-south-1"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
